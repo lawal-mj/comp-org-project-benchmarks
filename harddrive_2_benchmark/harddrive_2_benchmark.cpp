9d8c4ce45da1ebc9002e4271776c4f0f718111e4
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 08:01:30 -0400

    open file and load section data

--- harddrive_2_benchmark/harddrive_2_benchmark.cpp
+++ harddrive_2_benchmark/harddrive_2_benchmark.cpp
@@ -6,3 +6,13 @@ using namespace std::chrono;
 
 const long long total_bytes = 1e9;
 const int section_size = 10000;
+
+int main(){
+        ofstream outFile("sample.txt", ios::binary);
+
+    char section[section_size];
+    for (int i = 0; i < section_size; i++) {
+    section[i] = 'x';
+    }
+
+}
