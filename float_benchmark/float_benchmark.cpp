c8ff09c12a7129e71a32560babb4dd2cebd1577e
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 07:47:56 -0400

    initialize variables

--- float_benchmark/float_benchmark.cpp
+++ float_benchmark/float_benchmark.cpp
@@ -2,3 +2,14 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+int main() {
+        
+    const int billion = 1e9;
+
+    // Variables for operations
+    double a = 1.5;
+    double b = 2.5;
+    double result;
+
+}
