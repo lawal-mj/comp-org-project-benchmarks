104c8959714f1b2cf1e3f9adc2902093a4079ff8
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 07:51:49 -0400

    create variables

--- integer_benchmark/integer_benchmark.cpp
+++ integer_benchmark/integer_benchmark.cpp
@@ -2,3 +2,13 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+int main(){
+    const int billion = 1e9;
+
+    // Variables for operations
+    int32_t a = 2;
+    int32_t b = 2;
+    int32_t result;
+
+}
