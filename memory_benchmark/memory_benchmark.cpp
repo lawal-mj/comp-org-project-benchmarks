9329780110de73c67f99aa8f832bee1969a52ba6
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 07:54:42 -0400

    initialize starter variables

--- memory_benchmark/memory_benchmark.cpp
+++ memory_benchmark/memory_benchmark.cpp
@@ -2,3 +2,16 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+int main(){
+        // create array
+    const long long arraySize = 5000000000;
+
+    // int is 4 bytes at a time  
+    int* dataArray = new int[arraySize]; 
+    int temp_data;
+
+    auto start = high_resolution_clock::now();
+
+
+}
