d78f10ed7814a28a3d903018ed8fb304cb10bdaf
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 07:58:10 -0400

    load chars and start writing data

--- harddrive_1_benchmark/harddrive_1_benchmark.cpp
+++ harddrive_1_benchmark/harddrive_1_benchmark.cpp
@@ -6,3 +6,19 @@ using namespace std::chrono;
 
 const long long total_bytes = 1e9;
 const int section_size = 100;
+int main(){
+    ofstream outFile("sample.txt", ios::binary);
+
+    char section[section_size];
+    for (int i = 0; i < section_size; i++) {
+        section[i] = 'x';
+    }
+
+        // calculate writing time 
+    auto start_write = high_resolution_clock::now();
+    for (long long i = 0; i < total_bytes; i += section_size) {
+        outFile.write(section, section_size);
+    }
+
+
+}
