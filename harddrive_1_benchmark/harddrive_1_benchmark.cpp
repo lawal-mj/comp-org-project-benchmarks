9743e34ec5095556671d7b9719bfc4cd25c67da4
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-03-29 07:58:33 -0400

    finish gettting writing time

--- harddrive_1_benchmark/harddrive_1_benchmark.cpp
+++ harddrive_1_benchmark/harddrive_1_benchmark.cpp
@@ -19,6 +19,9 @@ int main(){
     for (long long i = 0; i < total_bytes; i += section_size) {
         outFile.write(section, section_size);
     }
+        auto end_write = high_resolution_clock::now();
+    auto WRITING_TIME = duration_cast<milliseconds>(end_write - start_write).count();
+    // finish getting writing time 
 
 
 }
